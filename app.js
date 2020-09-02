0f1dbe0cf211e6a350d9dc2b045e972d05199f37
kevin_p <kprsd14@gmail.com>
2020-09-02 15:34:36 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// LISTEN FOR SUBMIT
+
+document.getElementById('loan-form').addEventListener('submit', calculateResults);
+
+// Calculate results
+
+function calculateResults(e) {
+
+
+  e.preventDefault();
+}
