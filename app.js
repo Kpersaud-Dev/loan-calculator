65ca5c8b188fd97df3a8cbaaf78b6d573ac2de1a
kevin_p <kprsd14@gmail.com>
2020-09-02 18:21:11 -0400

    Added loading UI

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 // LISTEN FOR SUBMIT
 
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function (e) {
+  // Hide results
+  document.getElementById('results').style.display = 'none';
+
+  // Show Loader
+  document.getElementById('loading').style.display = 'block';
+
+  setTimeout(calculateResults, 2000);
+
+  e.preventDefault();
+});
 
 // Calculate results
 
-function calculateResults(e) {
+function calculateResults() {
   // UI Variables
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
@@ -25,16 +35,28 @@ function calculateResults(e) {
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+    // Show Results
+    document.getElementById('results').style.display = 'block';
+
+    // Hide Loader
+    document.getElementById('loading').style.display = 'none';
+
   } else {
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
 }
 
 // Show Error
 
 function showError(error) {
+  // Hide Results
+  document.getElementById('results').style.display = 'none';
+
+  // Hide Loader
+  document.getElementById('loading').style.display = 'none';
+
   //Create a div
   const errorDiv = document.createElement('div');
 
